aad736d4e79b21441c5c3a10c8c3b4cdc2ff2c33
flomartel <fjm25@kent.ac.uk>
2026-03-11 15:31:20 +0000

    user_menu_working

--- main_project_file.py
+++ main_project_file.py
@@ -1,13 +1,20 @@
 # Film rating, filtering, and watchlist system project main file
+from film import Film
+from watchlist import Watchlist
+
+watchlist = Watchlist()
+
+while True:
+
+    print("Welcome to your film organisation system!")
+    print("1. List Films")
+    print("2. Add Film")
+    print("3. Mark As Watched")
+    print("4. Rate Film")
+    print("5. Exit")
+
+    user_choice = input("Please choose an option: ")
 
 
-
-
-
-
-print("Welcome to your film organisation system!")
-print("1. List Films")
-print("2. Add Film")
-print("3. Mark As Watched")
-print("4. Rate Film")
-print("5. Exit") #just setting up the options i want for the project
+    if user_choice == "5":
+        break
